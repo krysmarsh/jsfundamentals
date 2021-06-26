ab9f3531716e97b1ccb20d28d93763d7f580e51c
Krystle Marshall <krystlemarshall@Krystles-Air.hsd1.in.comcast.net>
2021-06-25 22:52:14 -0400

    Day11

---
+++ ClassRoomChallenges/Day11/practice.js
@@ -0,0 +1,13 @@
+let grade = studentGrade
+let name = studentName
+
+function = { 
+    studentName = ("Krystle")
+    studentGrade =("A")
+
+}
+
+    console.log("studentName" $(`got a) "studentGrade" $(`in social studies)
+    ;
+
+    //HELP
